d1f5e06159a034a3816559701fd6626dbdf1ea37
Sanzhar <dos.0227@yandex.ru>
2022-12-04 13:51:34 +0600

    Task4 done

---
+++ Task_4/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 21 - HARD необязательная
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в N-мерном пространстве. 
+// Сначала задается N с клавиатуры, потом задаются координаты точек.
+try
+{
+    Console.WriteLine("Введите число, которое будет определять скольки мерное пространство");
+    int n = Convert.ToInt32(Console.ReadLine());
+    if (n < 0) Console.WriteLine("Введите неотрицательное и целое число");
+    int size = n;
+    int[] A = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        Console.WriteLine($"Введите {i + 1}-е значение координаты A");
+        A[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    int[] B = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        Console.WriteLine($"Введите {i + 1}-е значение координаты B");
+        B[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    double sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+
+        sum = sum + Math.Pow((B[i] - A[i]), 2);
+    }
+    double result = Math.Sqrt(sum);
+    Console.WriteLine($"Расстояние между координатами A и B равно = {result}");
+}
+
+catch
+{
+    Console.WriteLine("Надо ввести целое и неотрицательное число!");
+}
